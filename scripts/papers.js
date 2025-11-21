aa42decc992600029f32a1c4432b76e0b4a28612
ykawase5048 <yuto.kawase3435@gmail.com>
2025-11-22 04:00:14 +0900

    Dynamically render paper.html from JSON database

---
+++ scripts/papers.js
@@ -0,0 +1,98 @@
+fetch("../databases/papers.json")
+    .then(response => response.json())
+    .then(data => {
+        renderPublications(data.publications, "publications-container");
+        renderPreprints(data.preprints, "preprints-container");
+    });
+
+// Publications
+function renderPublications(list, containerId) {
+    const container = document.getElementById(containerId);
+
+    for (let i =  0; i < list.length; i++) {
+        const entry = list[i];
+        const number = list.length - i;
+
+        const div = document.createElement("div");
+        div.className = "pub-item";
+
+        div.innerHTML = `
+            <div class="pub-index">
+                <h4 class="font-weight-light mb-0">
+                    ${number}.
+                </h4>
+            </div>
+
+            <div>
+                <div class="pub-title">
+                    <h4 class="font-weight-light mb-0">
+                        ${entry.title}
+                        <a class="btn btn-outline-secondary btn-sm" href="https://doi.org/${entry.doi}" target="_blank">
+                            <div>
+                                <img src="./icons/paper_plane.svg" width="20" height="20">
+                                DOI
+                            </div>
+                        </a>
+                    </h4>
+                </div>
+
+                <div class="pub-info">
+                    <h6 class="font-weight-light">
+                        ${entry.journal}
+                        <span style="margin-left:0.2em;">
+                            -
+                        </span>
+                        <span style="margin-left:0.2em;">
+                            ${entry.year}
+                        </span>
+                    </h6>
+                </div>
+            </div>
+        `;
+        container.appendChild(div);
+    }
+}
+
+// Preprints
+function renderPreprints(list, containerId) {
+    const container = document.getElementById(containerId);
+
+    for (let i =  0; i < list.length; i++) {
+        const entry = list[i];
+        const number = list.length - i;
+        const dateWithNote = entry.note ? `${entry.date} (${entry.note})` : entry.date;
+
+        const div = document.createElement("div");
+        div.className = "pub-item";
+
+        div.innerHTML = `
+            <div class="pub-index">
+                <h4 class="font-weight-light mb-0">
+                    ${number}.
+                </h4>
+            </div>
+
+            <div>
+                <div class="pub-title">
+                    <h4 class="font-weight-light mb-0">
+                        ${entry.title}
+                        <a class="btn btn-outline-secondary btn-sm"
+                           href="${entry.arxiv}" target="_blank">
+                            <div>
+                                <img src="./icons/arxiv-logomark-small.svg" width="20" height="20">
+                                arXiv
+                            </div>
+                        </a>
+                    </h4>
+                </div>
+
+                <div class="pub-info">
+                    <h6 class="font-weight-light">
+                        ${dateWithNote}
+                    </h6>
+                </div>
+            </div>
+        `;
+        container.appendChild(div);
+    }
+}
